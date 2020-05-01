f0585707f50e24cbd83eeb9a932f91d9cf71a957
Saju Chacko Rajan <sajucrajan@gmail.com>
2020-04-30 22:56:07 -0400

    Capstone project template files added. Python Notebook and python module files.

---
+++ Group_1_THE_ONE_Module.py
@@ -0,0 +1,30 @@
+"""
+<Project Title>
+
+Copyright (c) 2019
+Licensed
+Written by <>
+"""
+
+def function1(var1,var2,var3):
+    """
+
+    :param var1:
+    :param var2:
+    :return:
+    """
+    total_sum = var1 + var2 + var3
+
+    return total_sum
+
+def function2(var1,var2,var3):
+    """
+
+    :param var1:
+    :param var2:
+    :param var3:
+    :return:
+    """
+    average = (var1 + var2 + var3) / 3
+    return average
+
